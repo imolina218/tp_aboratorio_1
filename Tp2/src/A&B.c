74524b277c16ffffd30f7f37ea98801f707b25a7
imolina218 <31362263+imolina218@users.noreply.github.com>
2021-05-15 22:28:47 -0300

    Add files via upload

---
+++ Tp2/src/A&B.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : A&B.c
+ Author      : Ismael Molina
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include "Menu.h"
+
+int main()
+{
+	setbuf(stdout,NULL);
+    mainMenu();
+
+    return 0;
+}
